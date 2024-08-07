eb25e3447243511451bc79a6182f060e8e534c41
Nelson Valverde La Torre <nelsonvalverdelt@aysconsultores.pe>
2024-08-06 19:19:42 -0500

    configuración de shell

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,10 @@ const { shareAll, withModuleFederationPlugin } = require('@angular-architects/mo
 module.exports = withModuleFederationPlugin({
 
   remotes: {
-    "mf-dashboard": "http://localhost:4201/remoteEntry.js",
-    "mf-products": "http://localhost:4202/remoteEntry.js",
-    "mf-orders": "http://localhost:4202/remoteEntry.js",
+    "mf-auth": "http://localhost:4201/remoteEntry.js",
+    "mf-dashboard": "http://localhost:4202/remoteEntry.js",
+    "mf-products": "http://localhost:4203/remoteEntry.js",
+    "mf-orders": "http://localhost:4204/remoteEntry.js",
   },
 
   shared: {

d62b2dec83df5f69ffbfa6bee0f5eb5fd3ae1b76
Nelson Valverde La Torre <nelsonvalverdelt@aysconsultores.pe>
2024-08-27 01:29:42 -0500

    Fix routing prod

--- src/app/decl.d.ts
+++ src/app/decl.d.ts
@@ -1,3 +1,4 @@
 declare module 'mf-dashboard/*';
 declare module 'mf-products/*';
 declare module 'mf-orders/*';
+declare module 'mf-auth/*';
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,1 +1,16 @@
-module.exports = require('./webpack.config');
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+
+  remotes: {
+    "mf-auth": "/auth/remoteEntry.js",
+    "mf-dashboard": "/dashboard/remoteEntry.js",
+    "mf-products": "/products/remoteEntry.js",
+    "mf-orders": "/orders/remoteEntry.js",
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+
+});

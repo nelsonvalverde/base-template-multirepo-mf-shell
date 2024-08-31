7751ccdfd5befbcfe8701fa37374f91618bfab3e
Nelson Valverde La Torre <nelsonvalverdelt@aysconsultores.pe>
2024-08-31 00:46:28 -0500

    eliminación de zone,js + zoneless agregado

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(),
     provideAnimationsAsync(),
     provideRouter(routes),
+    provideClientHydration(),
+    provideExperimentalZonelessChangeDetection()
   ]
 };
